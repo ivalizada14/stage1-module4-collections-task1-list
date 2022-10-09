7fe7151a5a8648f5fd9276e77d987e1bbdad3326
Ibrahim Velizade <ivalizada14@gmail.com>
2022-10-09 14:40:34 +0300

    second try

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -1,12 +1,13 @@
 package com.epam.mjc.collections.list;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
         LinkedList<Integer> linkedList = new LinkedList<>();
-        for(int i = 0; i<sourceList.size(); i++)
+        for(int i = sourceList.size()-1; i>=0; i--)
         {
             if(sourceList.get(i)%2==1)
                 linkedList.add(sourceList.get(i));
@@ -14,8 +15,10 @@ public class LinkedListCreator {
         for(int i = 0; i<sourceList.size(); i++)
         {
             if(sourceList.get(i)%2==0)
-                linkedList.addLast(sourceList.get(i));
+                linkedList.add(sourceList.get(i));
         }
+
         return linkedList;
     }
+
 }
