96b944b2b09cc4f89c42d57e3fa733e2f2577be3
Ibrahim Velizade <ivalizada14@gmail.com>
2022-10-09 14:25:46 +0300

    second try

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -9,9 +9,12 @@ public class LinkedListCreator {
         for(int i = 0; i<sourceList.size(); i++)
         {
             if(sourceList.get(i)%2==1)
-                linkedList.add(sourceList.get(i));
-            else
                 linkedList.addLast(sourceList.get(i));
+        }
+        for(int i = 0; i<sourceList.size(); i++)
+        {
+            if(sourceList.get(i)%2==0)
+                linkedList.add(sourceList.get(i));
         }
         return linkedList;
     }
